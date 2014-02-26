f99a995bdbdace1355aec72aea230549404bfa80
Andre Cunha <andre.lv.cunha@gmail.com>
2014-02-26 16:19:27 -0300

    Added docstring and check for lists and tuples in ilen.

--- utils.py
+++ utils.py
@@ -6,6 +6,11 @@ base_path = abspath(dirname(modules[__name__].__file__))
 
 
 def ilen(it):
+    """Calculate the number of elements in an iterable.
+    """
+    if isinstance(it, list) or isinstance(it, tuple):
+        return len(it)
+
     count = 0
     for i in it:
         count = count + 1
