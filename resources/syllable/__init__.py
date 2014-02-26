de7ff9b73710caf0b8f5850872b2bfaa9b058f4f
Andre Cunha <andre.lv.cunha@gmail.com>
2014-02-26 13:34:55 -0300

    Use absolute imports.

--- resources/syllable/__init__.py
+++ resources/syllable/__init__.py
@@ -1,4 +1,4 @@
-from .silva2011 import Silva2011SyllableSeparator
-from .ceci import CECISyllableSeparator
+from coh.resources.syllable.silva2011 import Silva2011SyllableSeparator
+from coh.resources.syllable.ceci import CECISyllableSeparator
 
 syllable_separator = CECISyllableSeparator()
