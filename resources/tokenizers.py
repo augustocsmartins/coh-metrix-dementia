41ab3fdc23c458dd1b4c029eda6d02a2d721a2bf
Andre Cunha <andre.lv.cunha@gmail.com>
2014-03-12 10:18:01 -0300

    Changed resources.senter to NLTK's default Portuguese tokenizer.

--- resources/tokenizers.py
+++ resources/tokenizers.py
@@ -3,6 +3,7 @@ from nltk.data import load
 from nltk.tag import OpenNLPPoSTagger
 from coh.utils import base_path
 
-senter = load(base_path + '/models/punkt/punkt-senter.pickle')
+#senter = load(base_path + '/models/punkt/punkt-senter.pickle')
+senter = load('tokenizers/punkt/portuguese.pickle')
 
 word_tokenize = nltk.word_tokenize
